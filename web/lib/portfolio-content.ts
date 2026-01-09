a98b705acb3030aef3142ce206e793fdbabed58b
d3ming <d3ming@gmail.com>
2026-01-08 16:16:04 -0800

    chore: reorganize about content into content/about and fix filenames

--- web/lib/portfolio-content.ts
+++ web/lib/portfolio-content.ts
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
 
-const contentDirectory = path.join(process.cwd(), "content/portfolio");
+const contentDirectory = path.join(process.cwd(), "content/about");
 
 export interface PortfolioSection {
     id: string;
